aeece781bf29a940094c666257ed2578003a3f13
v123582 <v123582@gmail.com>
2016-06-15 15:34:51 +0800

    New Feature: Facebook Login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,15 @@
                 </div>
             </div>
             <center>
-              <img src="./images/fblogin.jpg" width="30%">
-              <p style="color:#AAAAAA;margin-top:10px;">-OR-</p>
-            <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                @if (session()->has('message'))
+                    <div class="alert alert-danger">
+                        {!! session()->get('message') !!}
+                    </div>
+                @endif
+            <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+            </fb:login-button> 
+            <p style="color:#AAAAAA;margin-top:10px;">-OR-</p>
+            <form class="form-horizontal" name="myform" role="form" method="POST" action="{{ url('/login') }}">
                 {!! csrf_field() !!}
 
                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
@@ -44,10 +50,15 @@
                     </div>
                 </div>
 
+                <input type="hidden" name="isFbLogin" id="isFbLogin">
+                <input type="hidden" name="FbEmail" id="FbEmail">
+                <input type="hidden" name="FbName" id="FbName">
+                <input type="hidden" name="FbserialNumber" id="FbserialNumber">
+
                 <div class="form-group">
                     <div class="col-md-6 col-md-offset-4">
                         <button type="submit" class="btn btn-danger">
-                            <i class="fa fa-btn fa-sign-in"></i>START!
+                            <i class="fa fa-btn fa-sign-in"></i> 進入心願體驗歷程
                         </button>
 
                     </div>
@@ -59,3 +70,80 @@
     </div>
 </div>
 @endsection
+
+
+
+<script>  
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1721050088149127',
+      xfbml      : true,
+      version    : 'v2.6'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+      // This is called with the results from from FB.getLoginStatus().  
+        function statusChangeCallback(response) {  
+          // console.log('statusChangeCallback');  
+          // console.log(response);  
+          // The response object is returned with a status field that lets the  
+          // app know the current login status of the person.  
+          // Full docs on the response object can be found in the documentation  
+          // for FB.getLoginStatus().  
+          if (response.status === 'connected') {  
+            // Logged into your app and Facebook.  
+            testAPI();  
+          } else if (response.status === 'not_authorized') {  
+            // The person is logged into Facebook, but not your app.  
+            document.getElementById('status').innerHTML = 'Please log ' +  
+              'into this app.';  
+          } else {  
+            // The person is not logged into Facebook, so we're not sure if  
+            // they are logged into this app or not.  
+            document.getElementById('status').innerHTML = 'Please log ' +  
+              'into Facebook.';  
+          }  
+        }  
+          
+        // This function is called when someone finishes with the Login  
+        // Button.  See the onlogin handler attached to it in the sample  
+        // code below.  
+        function checkLoginState() {  
+          FB.getLoginStatus(function(response) {  
+            statusChangeCallback(response);  
+          });  
+        }  
+          
+        // Here we run a very simple test of the Graph API after login is  
+        // successful.  See statusChangeCallback() for when this call is made.  
+        function testAPI() {  
+          
+          FB.login(function(response) {
+            if (response.authResponse) {
+             console.log('Welcome!  Fetching your information.... ');
+             FB.api('/me', {fields: 'name,email'},  function(response) {
+                $('#isFbLogin').val('true');
+                $('#FbEmail').val(response.email);
+                $('#FbName').val(response.name);
+                $('#FbserialNumber').val(response.id);
+                console.log(response);
+                document.myform.submit();
+             });
+            } else {
+             alert('Facebook 登入失敗！');
+            }
+          }, {
+                scope: 'email', 
+                return_scopes: true
+            });
+        }  
+</script>  
