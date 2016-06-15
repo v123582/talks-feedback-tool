750d62f590d5dc009f69d8c8deba012ce6d2bd61
wunoodles <noodlesloves@gmail.conm>
2016-06-16 01:31:20 +0800

    'QQ'

--- app/Http/Controllers/SpeakerController.php
+++ app/Http/Controllers/SpeakerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use Auth;
+use DB;
 use Redirect;
 use App\User;
 use App\Speaker;
@@ -54,7 +55,24 @@ class SpeakerController extends Controller
    {
      $loginUserId = Auth::user()->id;
      $userVotes = Auth::user()->votes;
-     return view('result', compact('loginUserId','userVotes'));
+     $votes = DB::table('votes')->select(DB::raw('result, count(*) as count'))->where('user_id', $loginUserId)->groupby('result')->get();
+
+     foreach($votes as $index => $value) {
+       switch($value->result) {  //用中文取代投票result的編號
+         case "1": $value->result = '愛'; break;
+         case "2": $value->result = '使命感'; break;
+         case "3": $value->result = '智慧'; break;
+         case "4": $value->result = '實踐力'; break;
+         case "5": $value->result = '領導力'; break;
+         case "6": $value->result = '啟發'; break;
+         case "7": $value->result = '信心'; break;
+         case "8": $value->result = '創造力'; break;
+         case "9": $value->result = '其他'; break;
+       }
+     }
+    
+
+     return view('result', compact('loginUserId','userVotes','votes'));
    }
 
 }
--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -2,15 +2,16 @@
 
 @section('content')
 
+<div style="background-image:url('../images/final.png'); width:100%; height:500px;background-repeat: round;position: relative;">
 user : {{$loginUserId}}
+{{$votes}}
 
-@foreach($userVotes as $vote)
-{{$vote}}
-@endforeach
+
 
 <center>
-  <button style="margin-right:25px;" class="btn btn-primary" onclick="">分享到ＦＢ</button>
-  <button class="btn btn-danger" onclick="location.href='/'">回到主畫面</button>
+  <button style="margin-right:25px;margin-top:50px;" class="btn btn-primary" onclick="" >分享到ＦＢ</button>
+  <button style="margin-top:50px;" class="btn btn-danger" onclick="location.href='/'">回到主畫面</button>
 </center>
 
+</div>
 @endsection('content')
